426e59280e09bf348b85968d65d7f0be8b3f8b2d
Jakob Jordan <j.jordan@fz-juelich.de>
2016-06-06 22:09:48 +0200

    Add dummy files

---
+++ py/functions.py
@@ -0,0 +1,21 @@
+# *** ISUE ***: move imports to the top
+# *** ISUE ***: fix code style according to PEP8
+
+# *** ISUE ***: should return the square of x
+def square():
+
+    return 0
+
+# *** ISUE ***: implement you function you like
+def great_function( x ): 
+    return 0
+
+import numpy as np
+
+# *** ISUE ***: unbreak the class
+class Blubb(object):
+    def __init__(self):
+        x = 6
+        y = np.array([1,2,3])
+    def sum(self, y):
+        return self.y + x
